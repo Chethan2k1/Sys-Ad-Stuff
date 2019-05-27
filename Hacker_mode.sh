4311911903a9b40df2efd837a9e07e5aed185c5c
Chethan2k1 <crdy2k1@gmail.com>
2019-05-27 13:30:02 +0530

    V 2.0

--- Hacker_mode.sh
+++ Hacker_mode.sh
@@ -37,67 +37,67 @@ done
 
 for (( i = 1; i < 21; i++ )); do
 	touch logs0-intern${i}.txt
-cat server_logs.txt | grep " MasterH-Intern${i} ->" > logs0-intern${i}.txt
+cat server_logs.txt | grep "MasterH-Intern${i} ->" > logs0-intern${i}.txt
 input="./logs0-intern${i}.txt"
 while IFS= read -r line
 do
 	echo "MY name is ${line}"
-	d= echo "${LINE}" | grep MasterH-Intern${i} | cut -d " " -f4
+	d= echo "${LINE}" | grep "MasterH-Intern${i}" | cut -d " " -f4
 	path="/home/${d}"
-	su MasterH-Intern{i}
-	if  [ -r "${path}" ] ; then
+	su MasterH-Intern${i}
+	if  [ -x "${path}" ] ; then
 		echo "$LINE" > /home/MasterH-Intern${i}/Success.txt
     else
     	echo "$LINE" > /home/MasterH-Intern${i}/Failure.txt
 	fi  
 done < "$input"
-rm logs0-intern${i}
+rm logs0-intern${i}.txt
 done
 
 
 for (( i = 1; i < 21; i++ )); do
 	touch logs0-intern${i}.txt
-cat server_logs.txt | grep " Heisenberg-Intern${i} ->" > logs0-intern${i}.txt
+cat server_logs.txt | grep "Heisenberg-Intern${i} ->" > logs0-intern${i}.txt
 input="./logs0-intern${i}.txt"
 while IFS= read -r line
 do
 	echo "MY name is ${line}"
 	d= echo "${LINE}" | grep Heisenberg-Intern${i} | cut -d " " -f4
 	path="/home/${d}"
-	su Hertz-Intern{i}
-	if  [ -r "${path}" ] ; then
+	su Heisenberg-Intern${i}
+	if  [ -x "${path}" ] ; then
 		echo "$LINE" > /home/Heisenberg-Intern${i}/Success.txt
     else
     	echo "$LINE" > /home/Heisenber-Intern${i}/Failure.txt
 	fi  
 done < "$input"
-rm logs0-intern${i}
+rm logs0-intern${i}.txt
 done
 
 
 for (( i = 1; i < 21; i++ )); do
 	touch logs0-intern${i}.txt
-cat server_logs.txt | grep " Hertz-Intern${i} ->" > logs0-intern${i}.txt
+cat server_logs.txt | grep "Hertz-Intern${i} ->" > logs0-intern${i}.txt
 input="./logs0-intern${i}.txt"
 while IFS= read -r line
 do
 	echo "MY name is ${line}"
 	d= echo "${LINE}" | grep Hertz-Intern${i} | cut -d " " -f4
 	path="/home/${d}"
-	su Hertz-Intern{i}
-	if  [ -r "${path}" ] ; then
+	su Hertz-Intern${i}
+	if  [ -x "${path}" ] ; then
 		echo "$LINE" > /home/Hertz-Intern${i}/Success.txt
     else
     	echo "$LINE" > /home/Hertz-Intern${i}/Failure.txt
 	fi  
 done < "$input"
-rm logs0-intern${i}
+rm logs0-intern${i}.txt
 done
 
 
 for (( i = 1; i < 21; i++ )); do
 	touch logs0-intern${i}.txt
-cat server_logs.txt | grep " Holland-Intern${i} ->" > logs0-intern${i}.txt
+cat server_logs.txt | grep "Holland-Intern${i} ->" > logs0-intern${i}.txt
 
 input="./logs0-intern${i}.txt"
 while IFS= read -r line
@@ -105,19 +105,19 @@ do
 	echo "MY name is ${line}"
 	d= echo "${LINE}" | grep Holland-Intern${i} | cut -d " " -f4
 	path="/home/${d}"
-	su Hertz-Intern{i}
-	if  [ -r "${path}" ] ; then
+	su Holland-Intern${i}
+	if  [ -x "${path}" ] ; then
 		echo "$LINE" > /home/Holland-Intern${i}/Success.txt
     else
     	echo "$LINE" > /home/Holland-Intern${i}/Failure.txt
 	fi  
 done < "$input"
-rm logs0-intern${i}
+rm logs0-intern${i}.txt
 done
 
 
 touch logs0-MasterH.txt
-cat server_logs.txt | grep " MasterH ->" > logs0-MasterH.txt
+cat server_logs.txt | grep "MasterH ->" > logs0-MasterH.txt
 input="./logs0-MasterH.txt"
 while IFS= read -r line
 do
@@ -125,18 +125,18 @@ do
 	d= echo "${LINE}" | grep MasterH | cut -d " " -f4
 	path="/home/${d}"
 	su MasterH
-	if  [ -r "${path}" ] ; then
+	if  [ -x "${path}" ] ; then
 		echo "$LINE" > /home/MasterH/Success.txt
     else
     	echo "$LINE" > /home/MasterH/Failure.txt
 	fi  
 done < "$input"
-rm logs0-MasterH
+rm logs0-MasterH.txt
 
 
 rm logs0-Heisenberg.txt
 touch logs0-Heisenberg.txt
-cat server_logs.txt | grep " Heisenberg ->" > logs0-Heisenberg.txt
+cat server_logs.txt | grep "Heisenberg ->" > logs0-Heisenberg.txt
 input="./logs0-Heisenberg.txt"
 while IFS= read -r line
 do
@@ -144,17 +144,17 @@ do
 	d= echo "${LINE}" | grep Heisenberg | cut -d " " -f4
 	path="/home/${d}"
 	su Heisenberg
-	if  [ -r "${path}" ] ; then
+	if  [ -x "${path}" ] ; then
 		echo "$LINE" > /home/Heisenberg/Success.txt
     else
     	echo "$LINE" > /home/Heisenberg/Failure.txt
 	fi  
 done < "$input"
-rm logs0-Heisenberg
+rm logs0-Heisenberg.txt
 
 
 touch logs0-Hertz.txt
-cat server_logs.txt | grep " Hertz ->" > logs0-Hertz.txt
+cat server_logs.txt | grep "Hertz ->" > logs0-Hertz.txt
 input="./logs0-Hertz.txt"
 while IFS= read -r line
 do
@@ -162,17 +162,17 @@ do
 	d= echo "${LINE}" | grep Hertz | cut -d " " -f4
 	path="/home/${d}"
 	su Hertz
-	if  [ -r "${path}" ] ; then
+	if  [ -x "${path}" ] ; then
 		echo "$LINE" > /home/Hertz/Success.txt
     else
     	echo "$LINE" > /home/Hertz/Failure.txt
 	fi  
 done < "$input"
-rm logs0-Hertz
+rm logs0-Hertz.txt
 
 
 touch logs0-Holland.txt
-cat server_logs.txt | grep " Holland ->" > logs0-Holland.txt
+cat server_logs.txt | grep "Holland ->" > logs0-Holland.txt
 input="./logs0-Holland.txt"
 while IFS= read -r line
 do
@@ -180,31 +180,55 @@ do
 	d= echo "${LINE}" | grep Holland | cut -d " " -f4
 	path="/home/${d}"
 	su Holland
-	if  [ -r "${path}" ] ; then
+	if  [ -x "${path}" ] ; then
 		echo "$LINE" > /home/Holland/Success.txt
     else
     	echo "$LINE" > /home/Holland/Failure.txt
 	fi  
 done < "$input"
-rm logs0-Holland
-
-su 
+rm logs0-Holland.txt
 
 echo "  TimeStamp : Hostname : Number of Hits "
 for (( i = 1; i < 21; i++ )); do
-	 echo "`cat server_logs.txt | grep "MasterH-Intern${i} ->" | cut -d " " -f1` : `cat server_logs.txt | grep "MasterH-Intern${i} ->" | cut -d " " -f2` : `cat server_logs.txt | grep "MasterH-Intern${i} ->" `"
+	cat server_logs.txt | grep "MasterH-Intern${i} ->" > Random_logs.txt
+	count=0
+	input="./Random_logs.txt"
+	while IFS= read -r line; do
+		count=$(( count + 1 ))
+		echo " `echo ${line} | cut -d " " -f1` : `echo ${line} | cut -d " " -f2` : `echo ${count}`"
+	done < "$input"
+	rm Random_logs.txt
 done
 
 for (( i = 1; i < 21; i++ )); do
-	 echo "`cat server_logs.txt | grep "Heisenberg-Intern${i} ->" | cut -d " " -f1` : `cat server_logs.txt | grep "Heisenberg-Intern${i} ->" | cut -d " " -f2` : `cat server_logs.txt | grep "Heisenberg-Intern${i} ->" `" 
+	cat server_logs.txt | grep "Heisenberg-Intern${i} ->" > Random_logs.txt
+	count=0
+	input="./Random_logs.txt"
+	while IFS= read -r line; do
+		count=$(( count + 1 ))
+		echo " `echo ${line} | cut -d " " -f1` : `echo ${line} | cut -d " " -f2` : `echo ${count}`"
+	done < "$input"
+	rm Random_logs.txt
 done
 
 for (( i = 1; i < 21; i++ )); do
-	 echo "`cat server_logs.txt | grep "Hertz-Intern${i} ->" | cut -d " " -f1` : `cat server_logs.txt | grep "Hertz-Intern${i} ->" | cut -d " " -f2` : `cat server_logs.txt | grep "Hertz-Intern${i} ->" `" 
+	cat server_logs.txt | grep "Hertz-Intern${i} ->" > Random_logs.txt
+	count=0
+	input="./Random_logs.txt"
+	while IFS= read -r line; do
+		count=$(( count + 1 ))
+		echo " `echo ${line} | cut -d " " -f1` : `echo ${line} | cut -d " " -f2` : `echo ${count}`"
+	done < "$input"
+	rm Random_logs.txt
 done
 
 for (( i = 1; i < 21; i++ )); do
-	 echo "`cat server_logs.txt | grep "Holland-Intern${i} ->" | cut -d " " -f1` : `cat server_logs.txt | grep "Holland-Intern${i} ->" | cut -d " " -f2` : `cat server_logs.txt | grep "Holland-Intern${i} ->" `" 
+	cat server_logs.txt | grep "Holland-Intern${i} ->" > Random_logs.txt
+	count=0
+	input="./Random_logs.txt"
+	while IFS= read -r line; do
+		count=$(( count + 1 ))
+		echo " `echo ${line} | cut -d " " -f1` : `echo ${line} | cut -d " " -f2` : `echo ${count}`"
+	done < "$input"
+	rm Random_logs.txt
 done
-
-
