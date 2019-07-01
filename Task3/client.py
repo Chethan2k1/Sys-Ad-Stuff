b1cfd17b96e5c112e4ea19685cb5ba799c9b6f2e
Chethan2k1 <crdy2k1@gmail.com>
2019-07-01 13:44:24 +0530

    Some problem in the code

--- Task3/client.py
+++ Task3/client.py
@@ -2,6 +2,7 @@
 
 import socket
 import sys
+import threading
 
 name=input("Enter your Username : ")
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -11,14 +12,17 @@ port=1235
 s.connect((host,port))
 s.setblocking(False)
 
-while True:
-    msg=input('> ')
-    if msg:
-        new_msg='@'+name+'> '+msg
-        s.send(new_msg.encode('utf-8'))
-    try:
-        while True:
+def msg_input():
+    while True:
+        msg=input('> ')
+        if msg:
+            new_msg='@'+name+'> '+msg
+            s.send(new_msg.encode('utf-8'))
+
+def msg_read():
+    while True:
             data=s.recv(1024).decode('utf-8')
             print(data)
-    except:
-        pass
+    
+threading.Thread(target=msg_input,daemon=True).start()
+threading.Thread(target=msg_read,daemon=True).start()
--- Task3/server.py
+++ Task3/server.py
@@ -1,43 +1,53 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 import socket
+import threading
+import queue
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket.socket(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 host='127.0.0.1'
 port=1235
-s.bind((host,port))
-s.listen()
-sockets=[s]
+server.bind((host,port))
+server.listen()
 
-def recieve_message(client_socket):
-    try:
-        data=client_socket.recv(1024).decode('utf-8')
-        return data 
-    except:
-        False
+t=[]
+users=0
+print('Online:'+str(users)+'\n')
 
+def read(connection,q):
+    while True:
+        data=connection.recv(1024).decode('utf-8')
+        q.put(data)
 
-while True:
-    for socket in sockets:
-        client_socket, client_address=s.accept()
-        sockets.append([client_socket,client_address])
-        data=recieve_message(socket)
-        if data is False:
-            print(f"closed connection from {socket[1]}")
-            sockets.remove(socket)
-            continue
-        elif data!=None:
-            if(data[-4:]=='exit'): #Checking if we have typed in exit
-                socket.close()
-                sockets.remove(socket)
+def write(connection,q):
+    while True:
+        if(q.empty()==False):
+            data=q.get()
+            if len(data)!=0:
+                for a in t:
+                    if a!=connection:
+                        a.send(data.encode('utf-8'))
             else:
-                    print(data)
+                break
         else:
             continue
-        for client_socket in clients: #sending the all other sockets other than the one we are recieving data from
-            if client_socket!=socket:
-                client_socket.send(data.decode('utf-8'))
+    --users
+    print('Online:'+str(users)+'\n')
+    connection.close()
+
+
+def client_thread(connection):
+    ++users
+    print('Online:'+str(users)+'\n')
+    q=queue.Queue()
+    threading.Thread(target=read,args=(connection,q),daemon=True).start()
+    threading.Thread(target=write,args=(connection,q),daemon=True).start()    
+
+while True:
+        connection, address=server.accept()
+        t.append(connection)
+        threading.Thread(target=client_thread,args=(connection,),daemon=True).start()
 
 
             
