596dbeee1df16f85f64d3a7cac592da6e5916c3c
Chethan2k1 <crdy2k1@gmail.com>
2019-07-01 23:13:33 +0530

    corrected the code

--- Task3/server.py
+++ Task3/server.py
@@ -16,39 +16,53 @@ users=0
 print('Online:'+str(users)+'\n')
 
 def read(connection,q):
-    while True:
-        data=connection.recv(1024).decode('utf-8')
-        q.put(data)
+    try:
+        while True:
+            data=connection.recv(1024).decode('utf-8')
+            q.put(data)
+    except Exception:
+        pass
 
 def write(connection,q):
-    while True:
-        if(q.empty()==False):
-            data=q.get()
-            if len(data)!=0:
-                for a in t:
-                    if a!=connection:
-                        a.send(data.encode('utf-8'))
+    global users
+    try:
+        while True:
+            if(q.empty()==False):
+                data=q.get()
+                if len(data)!=0:
+                    for a in t:
+                        if a!=connection:
+                            a.send(data.encode('utf-8'))
+                else:
+                    break
             else:
-                break
-        else:
-            continue
-    --users
-    print('Online:'+str(users)+'\n')
-    connection.close()
-
-
+                continue
+    except Exception:
+        pass
+    finally:
+        print('client Diconnected')
+        users-=1
+        t.remove(connection)
+        print('Online:'+str(users)+'\n')
+        connection.close()
+        
 def client_thread(connection):
-    ++users
+    global users
+    users+=1
     print('Online:'+str(users)+'\n')
     q=queue.Queue()
     threading.Thread(target=read,args=(connection,q),daemon=True).start()
     threading.Thread(target=write,args=(connection,q),daemon=True).start()    
 
 while True:
+    try:
         connection, address=server.accept()
         t.append(connection)
         threading.Thread(target=client_thread,args=(connection,),daemon=True).start()
+    except:
+        break
 
+server.close()
 
             
 
